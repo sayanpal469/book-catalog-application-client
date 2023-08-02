c3a7b79b962bad02e2a989559587db4f13edf991
sayanpal469 <sayanpal469@gmail.com>
2023-08-02 12:20:23 +0530

    private route not done

---
+++ src/firebase.init.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDAKYGo6-sIMz7q9NnMVqx507-E1qeQuYk",
+  authDomain: "book-catalog-4408b.firebaseapp.com",
+  projectId: "book-catalog-4408b",
+  storageBucket: "book-catalog-4408b.appspot.com",
+  messagingSenderId: "1056994252166",
+  appId: "1:1056994252166:web:c7fe391dac95b30ccac807",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
